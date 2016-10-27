148a313f0aa1372f5f8445ad3c0cd02dd168154d
ReinoutEyckerman <reinouteyckerman@gmail.com>
2016-10-27 17:38:21 +0200

    Api JSON Update

--- src/com/company/MusicBrainzAPI.java
+++ src/com/company/MusicBrainzAPI.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import org.json.simple.JSONObject;
+
 /**
  * Created by reinout on 10/26/16.
  */
 public class MusicBrainzAPI {
+   public static void GetMetadata(String[] args){
+      JSONObject obj = new JSONObject();
 
+      obj.put("name", "foo");
+      obj.put("num", new Integer(100));
+      obj.put("balance", new Double(1000.21));
+      obj.put("is_vip", new Boolean(true));
+
+      System.out.print(obj);
+   }
 }
